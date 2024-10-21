2edef3b7c90e3546401625b42c0865ab0031d917
root <karimhabouchi310@gmail.com>
2024-10-21 09:58:42 +0000

    put

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,3 @@
 #!/bin/bash
- puts("Programming is like building a multilingual puzzle");
-    return (0);
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
+"Programming is like building a multilingual puzzle"
