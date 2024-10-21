5ebec808ad35cc99742badaf7862b3b41816f626
root <karimhabouchi310@gmail.com>
2024-10-21 13:31:31 +0000

    print

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -8,5 +8,6 @@
 int main(void)
 {
   
-  with proper grammar, but the outcome is a piece of art,  return (0);
+  printf ("with proper grammar, but the outcome is a piece of art,")
+    return (0);
 }
