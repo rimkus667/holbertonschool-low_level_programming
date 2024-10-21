4725b73cc5909885b67674292923cbd936689f68
root <karimhabouchi310@gmail.com>
2024-10-21 12:40:02 +0000

    put

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,15 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
-"Programming is like building a multilingual puzzle"
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints "Programming is like building a
+ * multilingual puzzle" to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+{
