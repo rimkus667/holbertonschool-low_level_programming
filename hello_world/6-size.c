00571a5a64f4f829321563bb0e12da41d3aa34ac
root <karimhabouchi310@gmail.com>
2024-10-21 14:27:20 +0000

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,13 +7,11 @@
  */
 int main(void)
 {
-    printf("Taille d'un int : %zu octets\n", sizeof(int));
-    printf("Taille d'un float : %zu octets\n", sizeof(float));
-    printf("Taille d'un double : %zu octets\n", sizeof(double));
-    printf("Taille d'un char : %zu octets\n", sizeof(char));
-    printf("Taille d'un short : %zu octets\n", sizeof(short));
-    printf("Taille d'un long : %zu octets\n", sizeof(long));
-    printf("Taille d'un long long : %zu octets\n", sizeof(long long));
+    printf("Size of a char: %zu byte(s)\n", sizeof(char));
+    printf("Size of an int: %zu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+    printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
+    printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
     return (0);
 }
