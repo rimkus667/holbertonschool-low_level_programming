c395ec2ca048f63d0f5acdcb69e4f92a4f169594
root <karimhabouchi310@gmail.com>
2024-10-22 14:26:17 +0000

    last

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,13 +3,11 @@
 #include <stdio.h>
 /* more headers goes there */
 /*betty style doc for function main goes there */
-
-/**                                                                                                                                                                                                                                       
- * main print the last digit of the number stored in the variable n                                                                                                                                                                                                     
- *                                                                                                                                                                                                                                        
- * Return: always 0 (Success)                                                                                                                                                                                                             
- */
-
+/**
+* main - print the last digit of the number stored in the variable n
+*
+* Return: Always 0 Success
+*/
 int main(void)
 {
 int n;
