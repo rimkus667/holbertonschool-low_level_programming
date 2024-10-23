905ce1b9ee28b224ef801157cd45ce1998140889
root <karimahbouchi@gmail.com>
2024-10-23 14:01:41 +0000

    num

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/* more headers goes there */
+/*betty style doc for function main goes there */
+/**
+* main - print the last digit of the number stored in the variable n
+*
+* Return: Always 0 Success
+*/
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+printf("%d", num);
+}
+printf("\n");
+return (0);
+}
