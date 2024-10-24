1243aea99ab8f675f5bfd86e1ed0db4fdcad781e
root <karimhabouchi310@gmail.com>
2024-10-24 13:57:22 +0000

    tnirp

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+/*betty style doc for function main goes there */
+/**
+* main - print the last digit of the number stored in the variable n
+*
+* Return: Always 0 Success
+*/
+int main(void)
+{
+char letter;
+for (letter = 'z'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+return (0);
+}
