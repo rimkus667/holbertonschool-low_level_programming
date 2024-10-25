3eac1eb985f717030867f096eb24bf8f35bb952c
root <karimhabouchi310@gmail.com>
2024-10-25 11:19:04 +0000

    print 09

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* more headers goes there */
+/*betty style doc for function main goes there */
+/**
+* main - prints all possible combinations of single-digit numbers
+* Return: Always 0 Success
+*/
+int main()
+{
+int i;
+for ( i = 0; i <= 9; i++)
+putchar(" %d ",i); 
+return 0;
+}
