d40ff50223ac10b1f2114d87c6b64c6da08f2295
root <karimhabouchi310@gmail.com>
2024-10-25 14:00:02 +0000

    printabc

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,17 +5,18 @@
 * main - prints all possible combinations of single-digit numbers
 * Return: Always 0 Success
 */
-int main()
+int main(void)
 {
 int i;
-for ( i = 0; i <= 9; i++)
-if (i < 9)
+for (i = 0; i <= 9; i++)
+{
 putchar(i + '0');
+if (i < 9)
 {
 putchar(',');
 putchar(' ');
 }
 }
 putchar('\n');
-return 0;
+return (0);
 }
