2a9d01bd705d39819ecb2a21c5125286648c9687
root <karimhabouchi310@gmail.com>
2024-10-28 15:23:19 +0000

    mul

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -3,7 +3,7 @@
  * mul - Multiplie deux entiers.
  * @a: premier entier a multiplier
  * @b: deuxieme entier a multiplier
- * Retourne : le produit des deux entiers.
+ * Retourne: le produit des deux entiers.
  */
 int mul(int a, int b)
 {
