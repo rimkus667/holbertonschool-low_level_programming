6a5d0191d5085dd4fb387899511c6672145e7365
root <karimhabouchi310@gmail.com>
2024-10-28 08:47:42 +0000

    alpha

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Entry print program
+ *
+ * Description: The program prints "_putchar", followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ *
+int(main)
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,6 +2,9 @@
 #define MYHEADER_H
 
 void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
 int _putchar(char c);
 int print_sign(int n);
 int _abs(int);
