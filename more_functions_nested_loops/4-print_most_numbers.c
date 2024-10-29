d1203cec21501eff6f14a77ad7e0df5bad186725
root <karimhabouchi310@gmail.com>
2024-10-29 10:58:32 +0000

    most

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers -  Affiche les chiffres de 0 a 9
+ * sauf 2 et 4 
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+if (i != 2 || i != 4)
+{
+_putchar(i + '0');}   
+_putchar('\n');
+}
