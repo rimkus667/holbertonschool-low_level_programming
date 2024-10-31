1eb1f27b4b414fc8a9d0708846055a87be8daf3c
root <karimhabouchi310@gmail.com>
2024-10-31 14:39:00 +0000

    fizzy

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,15 +9,15 @@ int main(void)
 int num;
 for (num = 1; num <= 100; num++)
 {
-if (num % 3 == 0)
+if (num % 3 == 0 && num % 5 == 0)
 {
 printf("Fizz ");
 }
-if (num % 5 == 0)
+else if (num % 3 == 0)
 {
 printf("Buzz ");
 }
-else if (num % 3 == 0 && num % 5 == 0)
+else if (num % 5 == 0)
 {
 printf("FizzBuzz ");
 }
