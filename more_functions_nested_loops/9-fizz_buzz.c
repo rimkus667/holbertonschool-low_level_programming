1a02691a0abc221b4f4a52e4ed8b81c61942a363
root <karimhabouchi310@gmail.com>
2024-10-31 14:27:09 +0000

    fizzy

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,11 +13,11 @@ if (num % 3 == 0)
 {
 printf("Fizz ");
 }
-if (num % 5 == 0)
+else if (num % 5 == 0)
 {
 printf("Buzz ");
 }
-if (num % 3 == 0 && num % 5 == 0)
+else if (num % 3 == 0 && num % 5 == 0)
 {
 printf("FizzBuzz ");
 }
