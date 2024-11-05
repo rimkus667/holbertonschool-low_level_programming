85a4b7362c072d285e41f5b021a94b2d928069ad
root <karimhabouchi310@gmail.com>
2024-11-05 08:58:39 +0000

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - echange
+ * @a: 1er
+ * @b: 2eme
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
+
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
