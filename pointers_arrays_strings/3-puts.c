4e29ab620291c29b39dd374447466a7c8475f028
root <karimhabouchi310@gmail.com>
2024-11-05 09:09:00 +0000

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - check the code
+ * @: varioable
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; str++)
+_putchar(str[i]);
+_putchar('\n');
+}
