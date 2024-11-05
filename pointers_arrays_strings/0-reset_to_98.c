993bafb7ec0357d8bf30deec96e1ea5892bbd9ae
root <karimhabouchi310@gmail.com>
2024-11-05 08:51:38 +0000

    reset

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - Met la valeur pointee par n a 98
+ * @n: Pointeur
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
-
+void reset_to_98(int *n);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
