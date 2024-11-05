96b4ce42c92a28f83476ea345c4b082d0d0e34f4
root <karimhabouchi310@gmail.com>
2024-11-05 09:05:52 +0000

    strlen

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
