d22764f3d90dfcf0a0b868cedbe4226a248c697a
root <karimhabouchi310@gmail.com>
2024-11-06 14:25:14 +0000

    str

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - Locate a character in a string
+ * @s: The string to search
+ * @c: The character to locate
+ * Return: A pointer to the first occurrence of the character c
+ *         in the string s, or NULL if the character is not found.
+ */
+char *_strchr(char *s, char c)
+{
+for (; *s >= '\0'; s++)
+{
+if (*s == c)
+{
+return (s);
+}
+}
+return (NULL);
+}
---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Locate a character in a string
+ * @s: The string to search
+ * @accept: The character to locate
+ * Return: A pointer to the first occurrence
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int count = 0
+int i, j;
+int found;
+for (i = 0; s[i] != '\0'; i++)
+{
+found = 0;
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+found = 1;
+count++;
+break;
+}
+}
+if (!found)
+{
+break;
+}
+}
+return (count);
+}
