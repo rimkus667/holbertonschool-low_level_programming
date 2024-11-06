a3155fe93ad902b033d5b4cc69c39397df9643e8
root <karimhabouchi310@gmail.com>
2024-11-06 14:34:05 +0000

    str

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,15 +1,13 @@
 #include "main.h"
-#include <stddef.h>
-
 /**
- * _strchr - Locate a character in a string
+ * _strspn - Locate a character in a string
  * @s: The string to search
  * @accept: The character to locate
  * Return: A pointer to the first occurrence
 */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int count = 0
+unsigned int count = 0;
 int i, j;
 int found;
 for (i = 0; s[i] != '\0'; i++)
