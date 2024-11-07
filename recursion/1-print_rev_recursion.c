da15693fab98c8da6e0a16cb26b0f72b153c7ce2
root <karimhabouchi310@gmail.com>
2024-11-07 15:30:17 +0000

    put

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -8,7 +8,9 @@
 void _print_rev_recursion(char *s)
 {
 if (*s == '\0')
+{
 return;
+}
 _print_rev_recursion(s + 1);
 putchar(*s);
 }
---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
