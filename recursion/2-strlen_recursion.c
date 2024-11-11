ff5f1c213939f92032f9b2919f7feb44ebb515b2
root <karimhabouchi310@gmail.com>
2024-11-11 16:57:18 +0000

    reverse

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,8 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
+ * _strlen_recursion - check the code
+ * @s: ss
  * Return: Always 0.
  */
 int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
+}
