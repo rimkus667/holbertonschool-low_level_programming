ac20ef1c0a1d83bcf526863421f54482bdcf5011
root <karimhabouchi310@gmail.com>
2024-11-12 14:47:09 +0000

    name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Affiche le nom du programme.
+* @argc: nom
+* @argv: Un tableau avec argv[0] comme nom du programme.
+ * Return: Toujours 0.
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
