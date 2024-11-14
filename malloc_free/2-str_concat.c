a8e0766318e6f82a28476a093b0e21b68058ba97
root <karimhabouchi310@gmail.com>
2024-11-14 13:37:11 +0000

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,21 +12,21 @@ char *chaine;
 int compteur1 = 0;
 int compteur2 = 0;
 int i;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 for(;s1[compteur1] != '\0'; compteur1++);
-{
-}
- for(;s2[compteur2] != '\0'; compteur2++);
-{
-}
+for(;s2[compteur2] != '\0'; compteur2++);
 chaine = (char *)malloc(compteur1 + compteur2 + 1);
 if (chaine == NULL)
 {
 return (NULL);
 }
-for (i = 0; i < compteur1; i++)
+for(i = 0; i < compteur1; i++)
 chaine[i] = s1[i];
-for (i = 0; i < compteur2; i++)
+for(i = 0; i < compteur2; i++)
 chaine[compteur1 + i] = s2[i];
 chaine[compteur1 + compteur2] = '\0';
-return (result);
+return (chaine);
 }
