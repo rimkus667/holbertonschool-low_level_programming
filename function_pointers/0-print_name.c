6efb0edcf43396b27210b02e3c6cc6558c2a6351
root <karimhabouchi310@gmail.com>
2024-11-19 15:02:12 +0000

    name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: char
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+{
+return;
+}
+f(name);
+}
