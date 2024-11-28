3f3e2b0ebb1301fd3f104ef4ff99082b300c4e50
root <karimhabouchi310@gmail.com>
2024-11-28 08:57:14 +0000

    var

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,10 +12,14 @@ va_list ap;
 unsigned int i;
 int sum = 0;
 if (n == 0)
-return 0;
-va_start (ap, n);
+return (0);
+{
+va_start(ap, n);
+{
 for (i = 0; i < n; i++)
-sum += va_arg (ap, int);
-va_end (ap);
-return sum;
+}
+sum += va_arg(ap, int);
+va_end(ap);
+return (sum);
+}
 }
