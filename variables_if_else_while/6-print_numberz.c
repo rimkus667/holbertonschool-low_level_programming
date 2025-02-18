647f66a8887b9ff351d6cdd188748c20c746b224
root <karimhabouchi310@gmail.com>
2025-02-18 15:31:10 +0000

    aabbcc

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int numb;
+for (numb = '0'; numb <= '9'; numb++)
+putchar (numb);
+putchar ('\n');
+return (0);
+}
