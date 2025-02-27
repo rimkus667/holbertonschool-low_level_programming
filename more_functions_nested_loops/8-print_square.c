e9a9b4065f558e0458756211f0b2b12130d0379b
root <karimhabouchi310@gmail.com>
2025-02-27 14:23:27 +0000

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - print square using the character '#'
+ * @size: The number of times '#' should be printed
+ * Return: void
+ */
+void print_square(int size)
+{
+int c, s;
+if (size == '0')
+{
+_putchar('\n');
+}
+for (c = 0; c < size; c++)
+{
+for (s = 0; s < size; s++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
