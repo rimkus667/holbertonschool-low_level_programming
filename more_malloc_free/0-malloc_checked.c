ea7bbbf8d8abf7cae92772e355ecd6921c037ca3
root <karimhabouchi310@gmail.com>
2025-03-13 11:18:31 +0000

    check

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: The number of bytes to allocate
+ *
+ * Return: Pointer to the allocated memory, or exit with status 98 if failed
+ */
+void *malloc_checked(unsigned int b)
+{
+    char *check;
+check = malloc(b);
+if (!check)
+{
+	exit(98);
+}
+return (check);
+}
