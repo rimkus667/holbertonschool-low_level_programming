409d103b5760fcaa4a7f315bcc6314113dbfcaf3
root <karimhabouchi310@gmail.com>
2025-03-13 09:51:03 +0000

    alloco

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,9 +11,34 @@
 int **alloc_grid(int width, int height)
 {
 	int a, b;
-	int **grille;
+	int **tab;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
+tab = malloc(height * sizeof(int *));
+if (tab ==  NULL)
+{
+	return (NULL);
+}
+for (a = 0; a < height; a++)
+{
+	tab[a] = malloc(width * sizeof(int));
+	if (tab[a] == NULL)
+	{
+		return (NULL);
+	for (b = 0; b < a; b++)
+	{
+		free(tab[b]);
+	}
+	free(tab);
+	return (NULL);
+	}
+			for (b = 0; b < width; b++)
+			{
+			tab[a][b] = 0;
+			}
+}
+	return (tab);
+}
