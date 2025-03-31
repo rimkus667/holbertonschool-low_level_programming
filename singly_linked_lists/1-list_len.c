fbd643341a731057d823160600db26c1ba20481e
root <karimhabouchi310@gmail.com>
2025-03-31 13:50:04 +0000

    List length

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * list_len - Returns the number of elements in a linked list_t list.
  * @h: Pointer to the head of the list.
@@ -8,13 +7,12 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h)
-    {
-        count++;
-        h = h->next;  /* Passe au nœud suivant */
-    }
-
-    return (count);
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
