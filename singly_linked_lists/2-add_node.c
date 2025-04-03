c58cc484557119498b2e331bd001f8a8fa18fb59
root <karimhabouchi310@gmail.com>
2025-04-03 13:11:31 +0000

    add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,27 +1,40 @@
 #include "lists.h"
 
 /**
- * add_node - Adds a new node at the beginning of a list_t list.
- * @head: Double pointer to the head of the list.
- * @str: String to duplicate and add to the new node.
+ * add_node - Adds a new node at the beginning of a list_t list
+ * @head: Double pointer to the head of the list
+ * @str: String to be duplicated and stored in the new node
  *
- * Return: Address of the new element, or NULL if it failed.
+ * Return: Address of the new node, or NULL if it fails
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	char *copy;
+	unsigned int len = 0;
+
+	if (!str)
+		return (NULL);
+
+	copy = strdup(str);
+	if (!copy)
+		return (NULL);
+
+
+	while (copy[len])
+		len++;
 
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
-		return (NULL);
-	new_node->str = strdup(str);
-	if (!new_node->str)
 	{
-		free(new_node);
+		free(copy);
 		return (NULL);
 	}
-	new_node->len = strlen(str);
+	new_node->str = copy;
+	new_node->len = len;
 	new_node->next = *head;
+
 	*head = new_node;
+
 	return (new_node);
 }
