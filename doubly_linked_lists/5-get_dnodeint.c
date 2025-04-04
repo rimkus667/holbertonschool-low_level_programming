c2f9623a874089e4e1edae0bfdf6045d22544f3c
root <karimhabouchi310@gmail.com>
2025-04-04 21:48:41 +0000

    get_dnodeint

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - Returns the nth node of a dlistint_t list
+ * @head: Pointer to the first node of the list
+ * @index: Index of the node to return (starting from 0)
+ *
+ * Return: Pointer to the node at index, or NULL if not found
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		if (i == index)
+		return (head);
+
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
