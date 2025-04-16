5e577c393123ef040718446357f6942cf01ba823
root <karimhabouchi310@gmail.com>
2025-04-16 14:10:20 +0000

    task 0v2

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -3,28 +3,25 @@
 /**
  * _strcat - Concatenates two strings
  * @dest: The destination string
- * @src The source string to append to dest
+ * @src: The source string to append to dest
  *
  * Return: A pointer to the resulting string dest
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
-
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
-
-    dest[i] = '\0';
-    
-    return (dest);
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
