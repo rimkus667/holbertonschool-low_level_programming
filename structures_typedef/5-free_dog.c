baffedf6a8e37c75229fda7a4c491e3ea7a3210d
root <karimhabouchi310@gmail.com>
2025-04-22 17:33:04 +0000

    task 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - Frees memory allocated for a dog_t struct
+ * @d: Pointer to the dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name)
+		free(d->name);
+
+	if (d->owner)
+		free(d->owner);
+
+	free(d);
+}
+
