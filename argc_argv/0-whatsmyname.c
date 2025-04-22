e3a5368d08c0c9a74349e204aab948107d55f8b1
root <karimhabouchi310@gmail.com>
2025-04-22 09:42:03 +0000

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the program name
+ * @argc: Argument count
+ * @argv: Array of arguments
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
